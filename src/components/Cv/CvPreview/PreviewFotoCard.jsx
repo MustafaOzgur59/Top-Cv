d9e6b1a1364eb8ca6bc2c8ed7457d21719f73752
Mozgur <dfaunm@gmail.com>
2022-08-04 18:36:30 +0300

    Fixed wrong property name usage for photo

--- src/components/Cv/CvPreview/PreviewFotoCard.jsx
+++ src/components/Cv/CvPreview/PreviewFotoCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PreviewFotoCard({ cv }) {
   return (
     <div className="PreviewFotoCard-container">
-      <img src={cv.photo} alt="" />
+      <img src={cv.personalInfo.photo} alt="" />
     </div>
   );
 }
